83721e2b7e62f2fc0bcef1b63ef4c2dd3b0ac56b
垚淼 <371128340@qq.com>
2022-05-04 16:59:45 +0800

    Create code_util.dart

---
+++ code_util.dart
@@ -0,0 +1,24 @@
+import 'dart:ffi';
+import 'dart:typed_data';
+
+
+class CodeUtil{
+
+  uint8PointerToUtf8(Pointer<Uint8> pUint8, int len){
+    Uint8List uint8list = pUint8.asTypedList(len);
+    Uint16List uint16list = Uint8ListToUint16List(uint8list);
+    String utf8 = String.fromCharCodes(uint16list);
+    return utf8;
+  }
+
+  Uint16List Uint8ListToUint16List(Uint8List uint8list){
+    int len = uint8list.length ~/ 2;
+    Uint16List uint16List = Uint16List(len);
+    for(int i=0; i<len; i++){
+      int val = (uint8list[i*2]&0xff) | (uint8list[i*2+1]&0xff)<<8 ;
+      uint16List[i] = val;
+    }
+    return uint16List;
+  }
+
+}
